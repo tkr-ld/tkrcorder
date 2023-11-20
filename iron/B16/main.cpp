1cfb72d79c5a82ee73bdc0dc3dad4c4689d1b131
sonobe takuro <tkr.ld.not.nyd@gmail.com>
2023-11-20 20:39:43 +0900

    B17

--- iron/B16/main.cpp
+++ iron/B16/main.cpp
@@ -1,29 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() { 
-    int N;
+vector<int> Answer;
 
-    cin >> N;
+int main() {
+  int N;
 
-    int h[N+1];
+  cin >> N;
 
-    for (int i = 1;i <= N;i++) {
-        cin >> h[i];
+  int h[N + 1];
+
+  for (int i = 1; i <= N; i++) {
+    cin >> h[i];
+  }
+
+  int dp[N + 1];
+
+  dp[1] = 0;
+  dp[2] = abs(h[2] - h[1]);
+
+  for (int i = 3; i <= N; i++) {
+    dp[i] =
+        min(abs(h[i] - h[i - 1]) + dp[i - 1], abs(h[i] - h[i - 2]) + dp[i - 2]);
+  }
+
+  int place = N;
+  while (true) {
+    Answer.push_back(place);
+    if (place = 1) break;
+    if (dp[place] == dp[place-1] + abs(h[place] - h[place - 1])) place = N-1;
+    else place = N -2;
+  };
+
+  reverse(Answer.begin(), Answer.end());
+
+  cout << Answer.size() << endl;
+  for (int i = 0; i < Answer.size(); i++) {
+    if (i != 0) {
+      cout << " ";
+      cout << Answer[i];
     }
+  }
 
-    int dp[N+1];
-
-    dp[1] = 0;
-    dp[2] = abs(h[2]-h[1]);
-
-
-    for (int i = 3;i <= N;i++) {
-        dp[i] = min(abs(h[i]-h[i-1])+dp[i-1], abs(h[i]-h[i-2])+dp[i-2]);
-    }
-
-
-    cout << dp[N] << endl;
-
-    return 0;
+  return 0;
 }
