1d37b7ad0870599ed4777f30b2585cc4497c815f
sonobe takuro <tkr.ld.not.nyd@gmail.com>
2023-11-20 23:48:57 +0900

    B19

--- iron/B18/main.cpp
+++ iron/B18/main.cpp
@@ -1,56 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main() { 
-    int N,S,A[69];
-    bool dp[69][100009];
+    long long N,W,w[109],v[109];
+    long long dp[109][1009];
+    cin >> N >> W;
+    for(int i = 1; i <= N; i++) cin >> w[i] >> v[i];
+    for (int i = 0; i <= N; i++) {
+		for (int j = 0; j <= 1000; j++) dp[i][j] = 1'000'000'000'000'000LL;
+	}
 
-    vector<long long> Answer;
+    dp[0][0] = 0;
 
-    cin >> N >> S;
-
-    for (int i = 1;i <= N;i++) cin >> A[i];
-    
-
-    dp[0][0] = true;
-    for (int i = 1;i <= S;i++) dp[0][i] = false;
-
-    for (int i = 1;i <= N;i++) {
-        for (int j = 0;j <= S;j++) {
-            dp[i][j] = false;
-
-            if (dp[i-1][j] == true) {
-                dp[i][j] = true;
-            } 
-
-            if (A[i] <= j && dp[i-1][j-A[i]] == true) {
-                dp[i][j] = true;
-                if (dp[i][j] && j == S) {
-                    int x = i;
-                    int y = j;
-                    while(x >= 0) {
-                        if(dp[x-1][y-A[x]]== true) {
-                            Answer.push_back(x);
-                            y = y - A[x];
-                        } else if (dp[x-1][y] == true) {
-                            // Answer.push_back(x);
-                        }
-                        x--;
-                    }
-                    cout << Answer.size() << endl;
-                    reverse(Answer.begin(), Answer.end());
-                    for (int i=0; i < Answer.size();i++) {
-                        if (i > 0) cout << " ";
-                        cout << Answer[i];
-                    }
-                    cout << endl;
-                    return 0;
-                }
-            } 
+    for (int i = 1; i <= N; i++) {
+		for (int j = 0; j <= 1000; j++) {
+            if(j < v[i] ){
+                dp[i][j] =  dp[i-1][j];
+            } else {
+                dp[i][j] = min(dp[i-1][j],dp[i-1][j-v[i]]+w[i]);
+            }
         }
+	}
+
+
+    long long Answer = 0;
+    for (int i = 0; i <= 1000; i++) {
+        if (dp[N][i] <= W) Answer = i;
     }
-
-    cout << "-1" << endl;
-
+    cout << Answer << endl;
     return 0;
 }
